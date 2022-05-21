4d5591ee082ead6b4901e0083a39ec701ec58d8c
bashh69 <bashh@hotmail.com>
2022-05-21 14:14:04 +0000

    fifth commit

--- src/js/views/planet.js
+++ src/js/views/planet.js
@@ -1,26 +1,31 @@
-import React, {useState, useEffect, useContext}from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import Link from "react-router-dom";
 
 export const Planet = (props) => {
-    console.log(props)
-    const { store, actions } = useContext(Context);
-    const fetchPlanet = "https://www.swapi.tech/api/"
-        useEffect(() => {
-        actions.loadPlanet(id)
-    }, [])
-    console.log(store)
-    let id = props.match.params.id
-    return (
-        <div>
-           <p>Name :{ store.planet.name }</p> 
-           <p>Diameter :{ store.planet.diameter }</p> 
-           <p>Rotation Period :{ store.planet.rotation_period }</p> 
-           <p>Orbital Period :{ store.planet.orbital_period }</p>
-           <p>Gravity :{ store.planet.gravity }</p>
-           <p>Population :{ store.planet.population }</p>
-           <p>Climate :{ store.planet.climate }</p>
-           <p>Terrain :{ store.planet.terrain }</p>
-           <p>Surface Water:{ store.planet.surface_water }</p>
-        </div>)
+  console.log(props);
+  const { store, actions } = useContext(Context);
+  const fetchPlanet = "https://www.swapi.tech/api/";
+  useEffect(() => {
+    actions.loadPlanet(id);
+  }, []);
+  console.log(store);
+  let id = props.match.params.id;
+  return (
+    <div className="planet">
+      <div className="card" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h1>Name :{store.planet.name}</h1>
+          <p>Diameter :{store.planet.diameter}</p>
+          <p>Rotation Period :{store.planet.rotation_period}</p>
+          <p>Orbital Period :{store.planet.orbital_period}</p>
+          <p>Gravity :{store.planet.gravity}</p>
+          <p>Population :{store.planet.population}</p>
+          <p>Climate :{store.planet.climate}</p>
+          <p>Terrain :{store.planet.terrain}</p>
+          <p>Surface Water:{store.planet.surface_water}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/js/views/starship.js
+++ src/js/views/starship.js
@@ -1,27 +1,31 @@
-import React, {useState, useEffect, useContext}from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import Link from "react-router-dom";
 
 export const Starship = (props) => {
-    console.log(props)
-    const { store, actions } = useContext(Context);
-    const fetchStarships = "https://www.swapi.tech/api/"
-        useEffect(() => {
-        actions.loadStarship(id)
-    }, [])
-    console.log(store)
-    let id = props.match.params.id
-    return (
-        <div>
-           <p>Name : { store.starship.name }</p>
-           <p>Model : { store.starship.model }</p>
-           <p>Class : { store.starship.starship_class }</p>
-           <p>Manufacturer : { store.starship.manufacturer }</p>
-           <p>Cost in Credits : { store.starship.cost_in_credits }</p>
-           <p>length : { store.starship.length }</p>
-           <p>Crew : { store.starship.crew }</p>
-           <p>Passengers : { store.starship.passengers }</p>
-           <p>Consumables : { store.starship.consumables }</p>   
-         
-      </div>)
+  console.log(props);
+  const { store, actions } = useContext(Context);
+  const fetchStarships = "https://www.swapi.tech/api/";
+  useEffect(() => {
+    actions.loadStarship(id);
+  }, []);
+  console.log(store);
+  let id = props.match.params.id;
+  return (
+    <div className="starship">
+      <div className="card" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h1>Name : {store.starship.name}</h1>
+          <p>Model : {store.starship.model}</p>
+          <p>Class : {store.starship.starship_class}</p>
+          <p>Manufacturer : {store.starship.manufacturer}</p>
+          <p>Cost in Credits : {store.starship.cost_in_credits}</p>
+          <p>length : {store.starship.length}</p>
+          <p>Crew : {store.starship.crew}</p>
+          <p>Passengers : {store.starship.passengers}</p>
+          <p>Consumables : {store.starship.consumables}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
